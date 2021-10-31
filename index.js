1a0a7ba4dff156fee325b6039875a37131394f20
israkkayum <israk.kayum@gmail.com>
2021-10-31 20:36:58 +0600

    fourth

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const { MongoClient } = require('mongodb');
+const ObjectId = require('mongodb').ObjectId;
+
 require('dotenv').config();
 const cors = require('cors');
 
@@ -19,13 +21,54 @@ async function run() {
         await client.connect();
         const database = client.db('foodDelivery');
         const categoriesCollection = database.collection('categories');
+        const restaurantCollection = database.collection('restaurant');
+        const teamsCollection = database.collection('teams')
+        const usersCollection = database.collection('users')
 
         app.get('/categories', async(req, res) => {
             const cursor = categoriesCollection.find({});
             const categories = await cursor.toArray();
             res.send(categories);
         });
+
+        app.get('/restaurant', async(req, res) => {
+            const cursor = restaurantCollection.find({});
+            const restaurant = await cursor.toArray();
+            res.send(restaurant);
+        });
+
+        app.post('/restaurant', async(req, res) => {
+            const newUser = req.body;
+            const result = await restaurantCollection.insertOne(newUser);
+            res.json(result);
+       });
+
+        app.get('/restaurant/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const product = await restaurantCollection.findOne(query);
+            res.json(product);
+
+        });
+
+        app.post('/users', async(req, res) => {
+            const newUser = req.body;
+            const result = await usersCollection.insertOne(newUser);
+            res.json(result);
+       });
+
+       app.get('/users', async(req, res) => {
+        const cursor = usersCollection.find({});
+        const users = await cursor.toArray();
+        res.send(users);
+    });
   
+        app.get('/teams', async(req, res) => {
+            const cursor = teamsCollection.find({});
+            const teams = await cursor.toArray();
+            res.send(teams);
+        });
+        
 
     }
 
